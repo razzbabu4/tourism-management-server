eda12f8de1e67452d06999217e10f9fcf6ee4f84
Tareq <razzbabut@gmail.com>
2024-04-29 04:29:59 +0600

    get data from mongodb

--- index.js
+++ index.js
@@ -36,13 +36,7 @@ async function run() {
             res.send(result)
         })
 
-        app.get('/myList/:email', async (req, res) => {
-            const email = req.params.email;
-            const query = { userEmail: email };
-            const cursor = touristSpotsCollection.find(query)
-            const result = await cursor.toArray();
-            res.send(result)
-        })
+        
 
         app.post('/touristSpots', async (req, res) => {
             const newTouristSpots = req.body;
@@ -51,12 +45,7 @@ async function run() {
             res.send(result)
         })
 
-        app.delete('/deleteSpots/:id', async(req,res)=>{
-            const id = req.params.id;
-            const query = {_id: new ObjectId(id)};
-            const result = await touristSpotsCollection.deleteOne(query);
-            res.send(result)
-        })
+        
 
 
 
