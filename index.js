5695ff46750a4b214b4eb38f5f8287c5f7e0f107
Tareq <razzbabut@gmail.com>
2024-04-29 04:53:30 +0600

    delete data from mongodb

--- index.js
+++ index.js
@@ -51,15 +51,12 @@ async function run() {
             res.send(result)
         })
 
-        // app.delete('/deleteSpots/:id', async(req,res)=>{
-        //     const id = req.params.id;
-        //     const query = {_id: new ObjectId(id)};
-        //     const result = await touristSpotsCollection.deleteOne(query);
-        //     res.send(result)
-        // })
-
-
-
+        app.delete('/deleteSpots/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await touristSpotsCollection.deleteOne(query);
+            res.send(result)
+        })
 
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
